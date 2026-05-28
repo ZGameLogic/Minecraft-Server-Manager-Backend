130d91fda36caf468e204a497ebe4e9d522eacf4
Ben Shabowski <benshabowski@gmail.com>
2026-05-28 09:07:20 -0500

    Add token to HTTP header cookie during authentication

--- src/main/java/com/zgamelogic/app/authentication/AuthenticationController.java
+++ src/main/java/com/zgamelogic/app/authentication/AuthenticationController.java
@@ -2,7 +2,11 @@ package com.zgamelogic.app.authentication;
 
 import com.fasterxml.jackson.annotation.JsonView;
 import com.zgamelogic.app.authentication.db.AuthenticationData;
+import com.zgamelogic.app.exceptions.InvalidMsmTokenException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,13 +18,25 @@ public class AuthenticationController {
 
     @JsonView(Views.AuthView.class)
     @PostMapping("/auth/code")
-    public AuthenticationData authorizeWithDiscordCode(@RequestBody CodeAuthenticationRequest req){
-        return authenticationService.authorizeWithDiscordCode(req.code(), req.redirectUrl());
+    public AuthenticationData authorizeWithDiscordCode(@RequestBody CodeAuthenticationRequest req, HttpServletResponse response){
+        AuthenticationData authData = authenticationService.authorizeWithDiscordCode(req.code(), req.redirectUrl());
+        Cookie authCookie = new Cookie("token", authData.getMsmToken());
+        authCookie.setHttpOnly(true);
+        authCookie.setPath("/");
+        authCookie.setMaxAge(60 * 60 * 24 * 365 * 10); // 10 years
+        response.addCookie(authCookie);
+
+        return authData;
     }
 
     @JsonView(Views.AuthView.class)
     @PostMapping("/auth/token")
-    public AuthenticationData authorizeWithMsmToken(@RequestBody String token){
+    public AuthenticationData authorizeWithMsmToken(
+            @RequestBody(required = false) String bodyToken,
+            @CookieValue(value = "token", required = false) String cookieToken
+    ){
+        String token = bodyToken != null && !bodyToken.isEmpty() ? bodyToken : cookieToken;
+        if(token == null || token.isEmpty()) throw new InvalidMsmTokenException();
         return authenticationService.authorizeWithMSMToken(token, true);
     }
 }
